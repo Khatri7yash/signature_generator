f46be9b3c9ca132106302baeb0aa120423022dba
Yash <khatri7yash@gmail.com>
2026-02-17 23:22:28 +0530

    - Adding Signature Generator (WIP).

--- app/src/main/java/com/signgntr/signaturegen/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/signgntr/signaturegen/presentation/screens/home/HomeScreen.kt
@@ -13,25 +13,28 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
 import com.signgntr.signaturegen.domain.common.Result
 import com.signgntr.signaturegen.presentation.common.BaseColumn
 import com.signgntr.signaturegen.presentation.common.BaseScreen
+import com.signgntr.signaturegen.presentation.navigation.Screen
 import com.signgntr.signaturegen.presentation.utils.annotation.ThemePreview
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavController) {
     val state by remember { mutableStateOf(Result.Success<Any>("")) }
     BaseScreen(title = "") {
         BaseColumn(uiState = state) {
-            Box{
-                ButtonScreen()
+            Box {
+                ButtonScreen(navController)
             }
         }
     }
 }
 
 @Composable
-private fun ButtonScreen() {
+private fun ButtonScreen(navController: NavController) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -42,7 +45,9 @@ private fun ButtonScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = {}) {
+            Button(onClick = {
+                navController.navigate(Screen.Signature.route)
+            }) {
                 Text(text = "Generate Signature")
             }
             Button(onClick = {}) {
@@ -55,5 +60,5 @@ private fun ButtonScreen() {
 @ThemePreview
 @Composable
 fun Preview() {
-    HomeScreen()
+    HomeScreen(navController = NavController(LocalContext.current))
 }
